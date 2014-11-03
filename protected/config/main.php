7adfde586a177aa59a3b08a2267bf8d69b3e607b
Coly <coly@live.cn>
2014-11-03 19:24:19 +0800

    注释

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,29 +1,16 @@
 <?php
-/*
-sadsadsaaaaaaaaa
-sadsa
-sdsd
-asdsadsdsdsd
-sdsdsdsdsdsdfdfgvbxcvb
-*/
-  'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-            'class'=>'WebUser',
-			'allowAutoLogin'=>true,
-            'loginUrl'=>array('path/login'),
-		),
-        'alipay'=>array(
-            'class'=>'application.vendors.alipay.AlipayProxy',
-            'partner'=>'2088801162173***', // your partner id
-            'key'=>'***om2l8gxuvca9gtniqbextf4y66***', // your key
-            'seller_email'=>'da***.li@***.com',
-            'return_url'=>'http://www.***.com/foo/returnAlipay',  
-            'notify_url'=>'http://www.***.com/foo/notifyAlipay',  //注意设置权限为Alipay可返回数据
-            'show_url'=>'http://www.yeedt.com/',
+return array(
+	'components'=>array(
+		/************************/
+        	'alipay'=>array(
+        	'class'=>'application.vendors.alipay.AlipayProxy',
+        	'partner'=>'2088801162173***', // your partner id
+        	'key'=>'***om2l8gxuvca9gtniqbextf4y66***', // your key
+        	'seller_email'=>'da***.li@***.com',
+        	'return_url'=>'http://www.***.com/foo/returnAlipay',  
+        	'notify_url'=>'http://www.***.com/foo/notifyAlipay',  //注意设置权限为Alipay可返回数据
+        	'show_url'=>'http://www.yeedt.com/',
         ),
-        /*sfddsfdsf
-        sdfdsfdsfsdfsdfsdfsdfds
-        */
+        /************************/
    ),
 ?>
